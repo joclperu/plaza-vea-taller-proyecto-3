2d0ffa494113378e3c529e3caa819d363f19b28c
Carlos Huamaní <carloshs92@gmail.com>
2016-05-02 09:32:48 -0500

    chore(sistema): commit inicial Se agrega el proyecto inicial

---
+++ Sistema Plaza Vea/SPV.DA/DataBaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+
+namespace SPV.DA
+{
+    public static class DataBaseHelper
+    {
+        public static string GetDbProvider()
+        {
+            return ConfigurationManager.ConnectionStrings["SPVConecctionString"].ProviderName;
+        }
+
+        public static string GetDbConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["SPVConecctionString"].ConnectionString;
+        }
+    }
+}
